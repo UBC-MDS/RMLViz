a47b84d79ce0e7f593c86ca45fc07c3fda7cec8d
Fanli Zhou <flizhou@gmail.com>
2020-02-26 18:35:50 -0800

    Added plot_train_valid_acc.R

---
+++ R/plot_train_valid_acc.R
@@ -0,0 +1,15 @@
+#' Plot training and validation dataset accuracies
+#'
+#' @param model_name a string of the machine learning model name
+#' @param train a data frame of the training data
+#' @param valid a data frame of the validation data
+#' @param param_name a string of the parameter name
+#' @param param_vec a vector of the parameter values
+#'
+#' @return A plot
+#' @export
+#' @examples
+#' plot_train_valid_acc(iris$Species)
+plot_train_valid_acc <- function(model_name, train, valid, param_name, param_vec) {
+
+}
