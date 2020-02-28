48fc098b37b1c6e45cadee487e29f4045b2bf82e
taohuang-ubc <tonyhuang0526ubc@gmail.com>
2020-02-28 15:47:33 -0800

    add docstring for plot_roc

--- R/plot_roc.R
+++ R/plot_roc.R
@@ -1,3 +1,14 @@
+#' creates a ROC plot
+#'
+#' Takes in a vector of prediction labels and a vector of prediction probabilities.
+#'
+#' @param y_label a vector contains the validation set label
+#' @param predict_prob a vector contains the prediction probabilities
+#'
+#' @return A plot
+#' @export
+#' @examples
+#' plot_roc(df$labels,df$predictions)
 plot_roc <- function(y_label,predict_prob) {
   roc(y_label,predict_prob,
       smoothed = TRUE,
