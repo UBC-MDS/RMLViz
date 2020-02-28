c1953171680c60f12206f5d5f08ca42b7b90246e
taohuang-ubc <tonyhuang0526ubc@gmail.com>
2020-02-28 15:34:50 -0800

    add function plot_roc

---
+++ R/plot_roc.R
@@ -0,0 +1,11 @@
+plot_roc <- function(y_label,predict_prob) {
+
+  roc(y_label,predict_prob,
+      smoothed = TRUE,
+      # arguments for ci
+      ci=TRUE, ci.alpha=0.9, stratified=FALSE,
+      # arguments for plot
+      plot=TRUE, auc.polygon=FALSE, max.auc.polygon=TRUE, grid=TRUE,
+      print.auc=TRUE, show.thres=TRUE)
+
+}
