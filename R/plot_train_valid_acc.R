3d7987d09a05aa0143d5ebf1a746553c1018948f
Fanli Zhou <flizhou@gmail.com>
2020-03-06 08:50:10 -0800

    First draft of plot_train_valid_acc

--- R/plot_train_valid_acc.R
+++ R/plot_train_valid_acc.R
@@ -1,22 +1,173 @@
-#' plot train/validation accuracies vs. parameter values
+library(tidyverse)
+library(ggplot2)
+library(class)
+library(rpart)
+library(e1071)
+library(randomForest)
+
+#' plot train/validation errors vs. parameter values
 #'
 #' Takes in a model name, train/validation data sets,
 #' a parameter name and a vector of parameter values
-#' to try and then plots train/validation accuracies vs.
+#' to try and then plots train/validation errors vs.
 #' parameter values.
 #'
-#' @param model_name a string of the machine learning model name
-#' @param X_train a data frame of the training dataset without labels
-#' @param y_train a data frame of the training labels
-#' @param X_valid a data frame of the validation dataset without labels
-#' @param y_valid a data frame of the validation labels
-#' @param param_name a string of the parameter name
-#' @param param_vec a vector of the parameter values
+#' @param model_name a string of the machine learning model name.
+#' Only 'knn', 'decision tree', 'svm', and 'random forests' are allowed.
+#' @param X_train a numeric data frame of the training dataset without labels.
+#' @param y_train a numeric vector or factor of the training labels.
+#' @param X_valid a numeric data frame of the validation dataset without labels.
+#' @param y_valid a numeric vector or factor of the validation labels.
+#' @param param_name a string of the parameter name.
+#' Please choose this parameter based on the following information:
+#' 'knn': 'k',
+#' 'decision tree': 'maxdepth',
+#' 'svm': 'cost' or 'gamma',
+#' 'random forests': 'ntree'.
+#' @param param_vec a numeric vector of the parameter values.
 #'
 #' @return A plot
 #' @export
 #' @examples
-#' plot_train_valid_acc("decision tree", X_train, y_train, X_valid, y_valid, "depth", c(1, 5, 10, 15, 20))
-plot_train_valid_acc <- function(model_name, X_train, y_train, X_valid, y_valid, param_name, param_vec) {
+#' plot_train_valid_error("decision tree", X_train, y_train, X_valid, y_valid, "maxdepth", c(1, 5, 10, 15, 20))
+plot_train_valid_error <- function(model_name, X_train, y_train, X_valid, y_valid, param_name, param_vec) {
 
+  if (!is.data.frame(X_train)){
+    stop("Sorry, X_train should be a data frame.")
+  }
+
+  if (!is.vector(y_train) && !is.factor(y_train)){
+    stop("Sorry, y_train should be a vector or factor.")
+  }
+
+  if (!is.data.frame(X_valid)){
+    stop("Sorry, X_valid should be a data frame.")
+  }
+  if (!is.vector(y_valid) && !is.factor(y_valid)){
+    stop("Sorry, y_valid should be a vector or factor.")
+  }
+
+  if (!all(purrr::map_lgl(X_train, is.numeric))){
+    stop("Sorry, all elements in X_train should be numeric.")
+  }
+
+  if (!is.numeric(y_train) && !is.factor(y_train)){
+    stop("Sorry, all elements in y_train should be numeric or factor.")
+  }
+
+  if (!all(purrr::map_lgl(X_valid, is.numeric))){
+    stop("Sorry, all elements in X_valid should be numeric.")
+  }
+
+  if (!is.numeric(y_valid) && !is.factor(y_valid)){
+    stop("Sorry, all elements in y_valid should be numeric or factor.")
+  }
+
+  if (!is.numeric(param_vec)){
+    stop("Sorry, all elements in para_vec should be numeric.")
+  }
+
+  if (any(param_vec < 0)){
+    stop("Sorry, all elements in para_vec should be non-negative.")
+  }
+
+  if (length(y_train) != nrow(X_train)){
+    stop("Sorry, X_train and y_train should have the same number of rows.")
+  }
+
+  if (length(y_valid) != nrow(X_valid)){
+    stop("Sorry, X_valid and y_valid should have the same number of rows.")
+  }
+
+  if (ncol(X_train) != ncol(X_valid)){
+    stop("Sorry, X_train and X_valid should have the same number of columns.")
+  }
+
+
+  data <- X_train
+  data$Target = y_train
+  df <- tibble::tibble(para = param_vec,
+                       train = rep(0, length(param_vec)),
+                       valid = rep(0, length(param_vec)))
+
+  model_name <- tolower(model_name)
+  param_name <- tolower(param_name)
+
+  for (i in seq(length(param_vec))){
+
+    if (model_name == 'knn'){
+
+      if (param_name != 'k'){
+        stop("Sorry, only the hyperparameter 'k' is allowed for a 'knn' model.")
+      }
+      y_train_pred <- class::knn(X_train, X_train, y_train, k = param_vec[i])
+      y_valid_pred <- class::knn(X_train, X_valid, y_train, k = param_vec[i])
+
+    } else if (model_name == 'decision tree'){
+
+      if (param_name != 'maxdepth'){
+        stop("Sorry, only the hyperparameter 'maxdepth' is allowed for a 'decision tree' model.")
+      }
+      classifier <- rpart::rpart(formula = Target ~ .,
+                                 data = data,
+                                 control = list(maxdepth = param_vec[i]))
+      y_train_pred <- stats::predict(classifier, newdata = X_train, type = 'class')
+      y_valid_pred <- stats::predict(classifier, newdata = X_valid, type = 'class')
+
+    } else if (model_name == 'svm') {
+
+      if (param_name == 'cost') {
+        classifier <- e1071::svm(formula = Target ~.,
+                                 data = data,
+                                 type = 'C-classification',
+                                 kernel = 'linear',
+                                 cost = param_vec[i])
+
+      } else if (param_name == 'gamma') {
+        classifier <- e1071::svm(formula = Target ~.,
+                                 data = data,
+                                 type = 'C-classification',
+                                 kernel = 'linear',
+                                 gamma = param_vec[i])
+
+      } else {
+        stop("Sorry, only the hyperparameters, 'cost' and 'gamma', are allowed for a 'svm' model.")
+      }
+
+      y_train_pred <- stats::predict(classifier, newdata = X_train, type = 'class')
+      y_valid_pred <- stats::predict(classifier, newdata = X_valid, type = 'class')
+
+    } else if (model_name == 'random forests') {
+
+      if (param_name != 'ntree') {
+        stop("Sorry, only the hyperparameter 'ntree' is allowed for a 'random forests' model.")
+      }
+
+      classifier <- randomForest::randomForest(x = X_train,
+                                               y = y_train,
+                                               ntree = param_vec[i])
+      y_train_pred <- stats::predict(classifier, newdata = X_train, type = 'class')
+      y_valid_pred <- stats::predict(classifier, newdata = X_valid, type = 'class')
+
+    } else {
+
+      stop("Sorry, the model_name should be chosen from 'knn', 'decision tree', 'svc' and 'random forests'.")
+    }
+
+    df$train[i] = sum(y_train_pred != y_train)/length(y_train)
+    df$valid[i] = sum(y_valid_pred != y_valid)/length(y_valid)
+  }
+
+
+  plot <- tidyr::gather(df, "dataset", "error", - para) %>%
+    ggplot2::ggplot(aes(para, error, color = dataset)) +
+    ggplot2::geom_line() +
+    ggplot2::labs(x = param_name,
+                  y = "Error",
+                  title = paste(model_name, "train and valid errors vs.", param_name)) +
+    ggplot2::theme(plot.title = element_text(size=16),
+                   axis.title = element_text(size=14),
+                   text = element_text(size=13))
+  plot
 }
+
