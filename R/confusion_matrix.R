36052fa211bf857688c833e559a1745be1e6af76
Mike Chen <mike_T_Chen@hotmail.com>
2020-03-06 21:52:01 -0800

    completed confusion matrix function

--- R/confusion_matrix.R
+++ R/confusion_matrix.R
@@ -2,20 +2,55 @@
 #' visual. If predicted_y array is passed in,other evaluation scoring metrics such as
 #' Recall, and precision will also be produced
 #'
-#' @param model trained model
-#' @param X_train a data frame of the training dataset without labels
-#' @param X_test a data frame of the validation dataset without labels
-#' @param y_train a data frame of the training labels
-#' @param y_test a data frame of the validation labels
+#' Implementation based on https://stackoverflow.com/a/59119854
+#'
+#' @param actual_y a data frame of the validation labels
 #' @param predicted_y a data frame of predicted values with model
 #' @param labels a list of string of confusion matrix labels
 #' @param title a string of the confusion matrix label
 #'
-#' @return A plot
+#' @return A plot and a dataframe with scoring metrics
 #' @export
 #'
 #' @examples
 #' confusion_matrix(svm, X_train, y_train, X_valid, y_valid)
-confusion_matrix <- function(model, X_train, X_test, y_train, y_test, predicted_y = NULL, labels = NULL, title = NULL){
+confusion_matrix <- function(actual_y, predicted_y, labels = NULL, title = NULL){
 
+  if (!is.vector(predicted_y) && !is.factor(predicted_y)){
+    stop("Sorry, y_valid should be a vector or factor.")
+  }
+
+  if (!is.vector(actual_y) && !is.factor(actual_y)){
+    stop("Sorry, y_valid should be a vector or factor.")
+  }
+
+  confusion_matrix <- confusionMatrix(predicted_y, actual_y)
+
+  table <- data.frame(confusion_matrix$table)
+
+
+
+
+  plot_table <- table %>%
+    mutate(goodbad = ifelse(table$Prediction == table$Reference, "good", "bad")) %>%
+    group_by(Reference) %>%
+    mutate(prop = Freq/sum(Freq))
+
+
+
+
+
+  confusion_plot <- ggplot(data = plot_table, mapping = aes(x = Reference, y = Prediction, fill = goodbad, alpha = prop)) +
+    geom_tile() +
+    geom_text(aes(label = Freq), vjust = .5, fontface  = "bold", alpha = 1) +
+    scale_fill_manual(values = c(good = "green", bad = "red")) +
+    theme_bw() +
+    xlim(rev(levels(table$Reference))) +
+    theme(legend.position = "none")
+
+  metric_score <- tidy(confusion_matrix$byClass)
+
+  print(confusion_plot)
+
+  return(metric_score)
 }
